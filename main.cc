0d46445a80730669790b94ca2e8b8bc1ae08bb87
hamza hany <hamzahany200731@gmail.com>
2026-04-27 22:23:11 +0300

    Learn_page

--- main.cc
+++ main.cc
@@ -45,6 +45,14 @@ int main() {
         auto resp = drogon::HttpResponse::newHttpViewResponse("SignInView");
         callback(resp);
     });
+
+    drogon::app().registerHandler("/dictionary",
+    [](const drogon::HttpRequestPtr &,
+       std::function<void(const drogon::HttpResponsePtr &)> &&callback) {
+        
+        auto resp = drogon::HttpResponse::newHttpViewResponse("DictionaryView");
+        callback(resp);
+    });
     // ... باقي الكود (addListener و run) ...
     std::cout << "Server is running... Open http://127.0.0.1:8080 in your browser." << std::endl;
     drogon::app().addListener("127.0.0.1", 8080);
